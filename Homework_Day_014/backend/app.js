5f215ff958a4bd83b9e02d0ba282d09796544385
Shahroz Gulliyev <safyur0621@gmail.com>
2023-08-04 21:41:57 +0500

    addd files

--- Homework_Day_014/backend/app.js
+++ Homework_Day_014/backend/app.js
@@ -1,25 +1,33 @@
-const express = require('express');
-const http = require('http');
-const {Server} = require('socket.io');
+const express = require('express')
+const http  = require('http')
+const {Server} = require('socket.io')
 
-const app = express();
-
-const server = http.createServer(app);
+const app = express()
+const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
         origin: '*',
-        methods: "*"
+        methods: ['GET', 'POST']
     }
-});
+})
+let users = []
+io.on('connection', socket => { 
+    users.push({id: socket.id, name: "test " + users.length})
+    const curUser = users.find(user => user.id === socket.id)
 
-io.on("connection", (socket) => {
-    
+    socket.on("message", (data) =>{
+        socket.broadcast.emit('response', {message: data.message, name: curUser.name});
+    })
+
+    socket.emit('name', curUser.name)
+
+    socket.on("disconnect", (data) =>{
+        users = users.filter(user => user.id!== socket.id)
+        // console.log("connection disconnected");
+    })
 })
 
-
-
-
-server.listen(8080, () => {
-    console.log('listening on *:8080');
-});
+server.listen(5555, () => {
+    console.log("PORT : 5555")
+})
