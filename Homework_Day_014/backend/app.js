a7fe0f6ecb6d31ae819775b1961171a2e6c27bd8
Shahroz Gulliyev <safyur0621@gmail.com>
2023-08-04 22:08:43 +0500

    addd files

--- Homework_Day_014/backend/app.js
+++ Homework_Day_014/backend/app.js
@@ -7,20 +7,21 @@ const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: '*',
-        methods: ['GET', 'POST']
+        origin: '*'
     }
 })
 let users = []
 io.on('connection', socket => { 
+    console.log(socket.id);
     users.push({id: socket.id, name: "test " + users.length})
     const curUser = users.find(user => user.id === socket.id)
 
-    socket.on("message", (data) =>{
-        socket.broadcast.emit('response', {message: data.message, name: curUser.name});
+    socket.on("submit", (data) =>{
+        io.emit('response', {message: data.message, name: curUser.name});
     })
 
     socket.emit('name', curUser.name)
+    socket.emit('users', users)
 
     socket.on("disconnect", (data) =>{
         users = users.filter(user => user.id!== socket.id)
