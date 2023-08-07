8a15434292ff00f80b8988e138de8525d7c2e9da
Shahroz Gulliyev <safyur0621@gmail.com>
2023-08-07 16:31:13 +0500

    assa

---
+++ Homework_Day_016/app.js
@@ -0,0 +1,19 @@
+const {Bot} = require('grammy');
+
+const distance = require('./distance');
+
+const bot = new Bot('6342700195:AAHxuLLYRUjKBWMh0Q_NYMQ9xNwa1HfS_Ek');
+
+bot.command('start', async (ctx) => {
+    await ctx.reply('Hello, Please enter your location: ', {
+        parse_mode: "HTML",
+    });
+});
+
+bot.on(':location', async (ctx) => {
+    const {latitude, longitude} = ctx.message.location;
+    const result = distance(latitude, longitude, latitude + 1, longitude + 1)
+    await ctx.reply(result)
+})
+
+bot.start();
